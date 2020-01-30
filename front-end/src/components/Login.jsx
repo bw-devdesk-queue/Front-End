efbd31f918088ef80939911232b03cf21b540125
Joshua Burleson <josh.burleson@bytes.co>
2020-01-29 22:52:07 -0500

    created 'toggle login-type' button

--- front-end/src/components/Login.jsx
+++ front-end/src/components/Login.jsx
@@ -8,10 +8,14 @@ import { login } from '../actions/actions';
 
 const Login = () => {
     const history = useHistory();
-    const rolePath = useRouteMatch().path.match(/admin/) ? 'admin' : 'user';
     const dispatch = useDispatch();
+    const rolePath = useRouteMatch().path.match(/admin/) ? 'admin' : 'user';
+    const oppositeRole = rolePath === 'admin' ? 'user' : 'admin';
+    
 
     const [loginData, setLoginData] = useState({username: '', password: ''});
+
+    const toggleLoginType = () => history.push(`/login/${oppositeRole}`);
 
     const handleChange = event => {
         setLoginData(
@@ -32,12 +36,14 @@ const Login = () => {
 
     return (
         <>
-        <h3>User Login</h3>
+        <h3>{rolePath[0].toUpperCase() + rolePath.slice(1)} Login</h3>
             <form onSubmit={handleSubmit}>
                 <input type="text" onChange={handleChange} value={loginData.username} name="username" placeholder="Username"></input>
                 <input type="password" onChange={handleChange} value={loginData.password} name="password" placeholder="Password"></input>
                 <input type="submit"></input>
             </form>
+        {/* Style this button to look like a toggle? */}
+        <button onClick={toggleLoginType}>Switch To {oppositeRole[0].toUpperCase() + oppositeRole.slice(1)} Login</button>
         </>
     );
 
