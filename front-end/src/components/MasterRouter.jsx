0fb474ef414a552dea09da22862318a9aa1820a3
David White <dwhite0627@gmail.com>
2020-01-30 21:53:46 -0500

    Add route for Tickets

--- front-end/src/components/MasterRouter.jsx
+++ front-end/src/components/MasterRouter.jsx
@@ -3,9 +3,11 @@ import { Route } from 'react-router-dom';
 
 import Login from './Login';
 import Register from './Register';
+import Tickets from './Tickets';
 
 export default () => <>
     <Route path="/login/user" component={Login} />
     <Route path="/login/admin" component={Login} />
     <Route path="/register" component={Register}/>
+    <Route path="/tickets" component={Tickets}/>
     </>;
