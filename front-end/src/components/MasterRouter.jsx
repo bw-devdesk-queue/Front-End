9a23980d3b8918e76c393257911fbe29c8388e66
David White <dwhite0627@gmail.com>
2020-02-01 17:43:52 -0500

    Add individual ticket routes for both user roles

--- front-end/src/components/MasterRouter.jsx
+++ front-end/src/components/MasterRouter.jsx
@@ -4,11 +4,22 @@ import { Route } from 'react-router-dom';
 import Login from './Login';
 import Register from './Register';
 import Tickets from './Tickets';
+import Ticket from './Ticket';
 
 export default () => <>
     <Route path="/login/user" component={Login} />
     <Route path="/login/admin" component={Login} />
     <Route path="/register" component={Register}/>
-    <Route path="/user/tickets" component={Tickets}/>
-    <Route path="/admin/tickets" component={Tickets}/>
+    <Route exact path="/user/tickets" component={Tickets}/>
+    <Route exact path="/admin/tickets" component={Tickets}/>
+    <Route 
+        exact
+        path='/user/tickets/:id' 
+        render={props => <Ticket {...props} />} 
+    />
+    <Route 
+        exact
+        path='/admin/tickets/:id' 
+        render={props => <Ticket {...props} />} 
+    />
     </>;
