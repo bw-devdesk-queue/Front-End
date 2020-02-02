c231ef5206c88674e7c90dc33eba1e2b3e662b4f
David White <dwhite0627@gmail.com>
2020-02-02 00:39:04 -0500

    Rename attributes of the Ticket data model

--- front-end/src/testData.js
+++ front-end/src/testData.js
@@ -1,35 +1,39 @@
 const userTicketsTest = [
   {
-    id: "1", 
+    ticket_id: "1", 
     title: "Unable to push to Lambda repo", 
     submitter: "David L White", 
     status: "unresolved", 
     helper: "Miguel Torres",
-    description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Expedita nobis saepe atque quo accusantium, repudiandae eius cumque minus, dignissimos eveniet impedit adipisci! Officia recusandae sit explicabo corrupti mollitia qui tempore? Lorem ipsum dolor, sit amet consectetur adipisicing elit. Expedita nobis saepe atque quo accusantium, repudiandae eius cumque minus, dignissimos eveniet impedit adipisci! Officia recusandae sit explicabo corrupti mollitia qui tempore? Lorem ipsum dolor, sit amet consectetur adipisicing elit. Expedita nobis saepe atque quo accusantium, repudiandae eius cumque minus, dignissimos eveniet impedit adipisci! Officia recusandae sit explicabo corrupti mollitia qui tempore? Lorem ipsum dolor, sit amet consectetur adipisicing elit. Expedita nobis saepe atque quo accusantium, repudiandae eius cumque minus, dignissimos eveniet impedit adipisci! Officia recusandae sit explicabo corrupti mollitia qui tempore? Lorem ipsum dolor, sit amet consectetur adipisicing elit. Expedita nobis saepe atque quo accusantium, repudiandae eius cumque minus, dignissimos eveniet impedit adipisci! Officia recusandae sit explicabo corrupti mollitia qui tempore? Lorem ipsum dolor, sit amet consectetur adipisicing elit. Expedita nobis saepe atque quo accusantium, repudiandae eius cumque minus, dignissimos eveniet impedit adipisci! Officia recusandae sit explicabo corrupti mollitia qui tempore? Lorem ipsum dolor, sit amet consectetur adipisicing elit. Expedita nobis saepe atque quo accusantium, repudiandae eius cumque minus, dignissimos eveniet impedit adipisci! Officia recusandae sit explicabo corrupti mollitia qui tempore?"
+    description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Expedita nobis saepe atque quo accusantium, repudiandae eius cumque minus, dignissimos eveniet impedit adipisci! Officia recusandae sit explicabo corrupti mollitia qui tempore? Lorem ipsum dolor, sit amet consectetur adipisicing elit. Expedita nobis saepe atque quo accusantium, repudiandae eius cumque minus, dignissimos eveniet impedit adipisci! Officia recusandae sit explicabo corrupti mollitia qui tempore? Lorem ipsum dolor, sit amet consectetur adipisicing elit. Expedita nobis saepe atque quo accusantium, repudiandae eius cumque minus, dignissimos eveniet impedit adipisci! Officia recusandae sit explicabo corrupti mollitia qui tempore? Lorem ipsum dolor, sit amet consectetur adipisicing elit. Expedita nobis saepe atque quo accusantium, repudiandae eius cumque minus, dignissimos eveniet impedit adipisci! Officia recusandae sit explicabo corrupti mollitia qui tempore? Lorem ipsum dolor, sit amet consectetur adipisicing elit. Expedita nobis saepe atque quo accusantium, repudiandae eius cumque minus, dignissimos eveniet impedit adipisci! Officia recusandae sit explicabo corrupti mollitia qui tempore? Lorem ipsum dolor, sit amet consectetur adipisicing elit. Expedita nobis saepe atque quo accusantium, repudiandae eius cumque minus, dignissimos eveniet impedit adipisci! Officia recusandae sit explicabo corrupti mollitia qui tempore? Lorem ipsum dolor, sit amet consectetur adipisicing elit. Expedita nobis saepe atque quo accusantium, repudiandae eius cumque minus, dignissimos eveniet impedit adipisci! Officia recusandae sit explicabo corrupti mollitia qui tempore?",
+    attempted_solution: ["Tried to lorem ipsum but couldn't sit amet consectetur", "After expedita nobis, the saepe atque didn't quo accusantium"]
   },
   {
-    id: "2", 
+    ticket_id: "2", 
     title: "Fix version 3.2 compatibility issues with jQuery plugin in Chrome", 
     submitter: "Josh", 
     status: "unresolved", 
     helper: "Shelbie",
-    description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Expedita nobis saepe atque quo accusantium, repudiandae eius cumque minus, dignissimos eveniet impedit adipisci! Officia recusandae sit explicabo corrupti mollitia qui tempore?"
+    description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Expedita nobis saepe atque quo accusantium, repudiandae eius cumque minus, dignissimos eveniet impedit adipisci! Officia recusandae sit explicabo corrupti mollitia qui tempore?",
+    attempted_solution: ["Tried to lorem ipsum but couldn't sit amet consectetur", "After expedita nobis, the saepe atque didn't quo accusantium"]
   },
   {
-    id: "3", 
+    ticket_id: "3", 
     title: "Mobile styles not fully responsive at <350px", 
     submitter: "Ramy", 
     status: "unresolved", 
     helper: "",
-    description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Expedita nobis saepe atque quo accusantium, repudiandae eius cumque minus, dignissimos eveniet impedit adipisci! Officia recusandae sit explicabo corrupti mollitia qui tempore?"
+    description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Expedita nobis saepe atque quo accusantium, repudiandae eius cumque minus, dignissimos eveniet impedit adipisci! Officia recusandae sit explicabo corrupti mollitia qui tempore?",
+    attempted_solution: ["Tried to lorem ipsum but couldn't sit amet consectetur", "After expedita nobis, the saepe atque didn't quo accusantium"]
   },
   {
-    id: "4", 
+    ticket_id: "4", 
     title: "Fix broken URL on Lambda.com sitemap", 
     submitter: "Jarvis", 
     status: "unresolved", 
     helper: "",
-    description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Expedita nobis saepe atque quo accusantium, repudiandae eius cumque minus, dignissimos eveniet impedit adipisci! Officia recusandae sit explicabo corrupti mollitia qui tempore?"
+    description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Expedita nobis saepe atque quo accusantium, repudiandae eius cumque minus, dignissimos eveniet impedit adipisci! Officia recusandae sit explicabo corrupti mollitia qui tempore?",
+    attempted_solution: ["Tried to lorem ipsum but couldn't sit amet consectetur", "After expedita nobis, the saepe atque didn't quo accusantium"]
   }
 ];
 
