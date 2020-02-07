7d3099786fb6bfcb4893bcf00804f025548a64b4
Joshua Burleson <josh.burleson@bytes.co>
2020-02-06 19:16:39 -0500

    Fixing landing for deploy

--- src/components/MasterRouter.jsx
+++ src/components/MasterRouter.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Redirect } from 'react-router-dom';
+import { Route, Redirect, Switch } from 'react-router-dom';
 import { PrivateRoute } from '../utils/PrivateRoute';
 
 import Login from './Login';
@@ -14,29 +14,32 @@ import UpdateForm from "./UpdateTicket"
 import GetSpecificTicket from "./GetSpecificTicket"
 
 export default () => <>
-    <Route exact path="/login" render={() => <Redirect to="/login/user" />} />
-    <Route path="/login/*" component={Login} />
-    <Route path="/register/*" component={Register}/>
-    <Route exact path="/user/tickets" component={Tickets}/>
-    <Route exact path="/admin/tickets" component={Tickets}/>
-    <Route exact path="/home" component={Home} />
-    <PrivateRoute exact path="/home/user/:id" component={UserHome}/>
-    <Route path="/home/user/:id/add-ticket" component={TicketForm}/>
-    
-    <Route  path="/home/user/:id/ticket/:id" component={GetSpecificTicket}/>
-    <Route exact path="/home/user/:id/ticket/:id" component={UpdateForm}/>
+    <Switch>
+        <PrivateRoute exact path='/' component={Tickets} />
+        <Route exact path="/login" render={() => <Redirect to="/login/user" />} />
+        <Route path="/login/*" component={Login} />
+        <Route path="/register/*" component={Register}/>
+        <Route exact path="/user/tickets" component={Tickets}/>
+        <Route exact path="/admin/tickets" component={Tickets}/>
+        <Route exact path="/home" component={Home} />
+        <PrivateRoute exact path="/home/user/:id" component={UserHome}/>
+        <Route path="/home/user/:id/add-ticket" component={TicketForm}/>
+        
+        <Route  path="/home/user/:id/ticket/:id" component={GetSpecificTicket}/>
+        <Route exact path="/home/user/:id/ticket/:id" component={UpdateForm}/>
 
-    {/* Tests */}
-    <Route path="/test" component={TestComp}/>
+        {/* Tests */}
+        <Route path="/test" component={TestComp}/>
 
-    <Route 
-        exact
-        path='/user/tickets/:id' 
-        render={props => <Ticket {...props} />} 
-    />
-    <Route 
-        exact
-        path='/admin/tickets/:id' 
-        render={props => <Ticket {...props} />} 
-    />
+        <Route 
+            exact
+            path='/user/tickets/:id' 
+            render={props => <Ticket {...props} />} 
+        />
+        <Route 
+            exact
+            path='/admin/tickets/:id' 
+            render={props => <Ticket {...props} />} 
+        />
+    </Switch>
     </>
