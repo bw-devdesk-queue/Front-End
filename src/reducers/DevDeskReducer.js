7defb06363d99883a27d06b273b89812f78a25eb
David White <dwhite0627@gmail.com>
2020-02-09 11:06:27 -0500

    Remove console logs from the 'UPDATE_TICKET' reducer action

--- src/reducers/DevDeskReducer.js
+++ src/reducers/DevDeskReducer.js
@@ -71,9 +71,6 @@ export const devDeskReducer = (state = initialState, action) => {
 
         
         case 'UPDATE_TICKET':
-                console.log("Updating state via reducer UPDATE_TICKET, payload=", action.payload)
-                console.log(`Updating state via reducer UPDATE_TICKET, tickets without ID=${action.payload.ticket_id}`, [ state.user.userTickets.filter( ticket => parseInt(ticket?.ticket_id) !== parseInt(action.payload?.ticket_id)) ])
-                console.log(`Updating state via reducer UPDATE_TICKET, tickets with ID=${action.payload.ticket_id}`, [ ...state.user.userTickets.filter( ticket => ticket?.ticket_id !== action.payload?.ticket_id), action.payload ])
                 return {...state,  user: { ...state.user, tickets: [ ...state.user.userTickets.filter( ticket => ticket?.ticket_id !== action.payload?.ticket_id), action.payload ]}}
 
         case 'ADD_TICKET':
